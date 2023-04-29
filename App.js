abda29ef668d9270dd76c4b18ac903e6594a0974
Jordan-Burgess <alfredjordan93@gmail.com>
2023-04-29 17:01:01 -0400

    Add Basic Register Page

--- App.js
+++ App.js
@@ -2,9 +2,10 @@ import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
 import AppIntro from './pages/AppIntro';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 export default function App() {
   return (
-    <Login/>
+    <Register/>
   );
 };
---
+++ pages/Register.js
@@ -0,0 +1,58 @@
+import {useState} from 'react';
+import {View, TextInput, Button, StyleSheet} from 'react-native';
+import Icon from 'react-native-vector-icons/Ionicons';
+
+
+function Register() {
+  const [email, setEmail] = useState('');
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+  const [passwordConfirm, setPasswordConfirm] = useState('');
+  const [hidePass, setHidePass] = useState(true)
+
+  return (
+    <View>
+      <TextInput
+        style={styles.input}
+        onChangeText={setEmail}
+        value={email}
+        placeholder='Enter Email'
+        inputMode='email'
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setUsername}
+        value={username}
+        placeholder='Enter Username'
+      />
+      <TextInput
+        style={styles.input}
+        onChangeText={setPassword}
+        value={password}
+        placeholder='Enter Password'
+        secureTextEntry={hidePass? true : false}
+      />
+      <Icon name={hidePass ? 'eye' : 'eye-off'} onPress={() => setHidePass(!hidePass)}/>
+      <TextInput
+        style={styles.input}
+        onChangeText={setPasswordConfirm}
+        value={passwordConfirm}
+        placeholder='Enter Password'
+        secureTextEntry={true}
+      />
+      <Button title='Register' color='#000'/>
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+    input: {
+      height: 40,
+      marginTop: 100,
+      margin: 12,
+      borderWidth: 1,
+      padding: 10,
+    },
+  });
+
+export default Register
