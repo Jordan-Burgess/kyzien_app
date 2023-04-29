a7a3b47812497b019c95c29745148a2cb02974f6
Jordan-Burgess <alfredjordan93@gmail.com>
2023-04-29 14:30:11 -0400

    Add AppIntro Component

--- pages/AppIntro.js
+++ pages/AppIntro.js
@@ -1,9 +1,142 @@
-import React from 'react'
+import React, { useState } from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  View,
+  Text,
+  Image,
+  Button,
+} from 'react-native';
+import AppIntroSlider from 'react-native-app-intro-slider';
 
-function AppIntro() {
+const AppIntro = () => {
+  const [showRealApp, setShowRealApp] = useState(false);
+
+  const onDone = () => {
+    setShowRealApp(true);
+  };
+
+  const onSkip = () => {
+    setShowRealApp(true);
+  };
+
+  const RenderItem = ({ item }) => {
+    return (
+      <View
+        style={{
+          flex: 1,
+          backgroundColor: item.backgroundColor,
+          alignItems: 'center',
+          justifyContent: 'space-around',
+          paddingBottom: 100,
+        }}>
+        <Text style={styles.introTitleStyle}>{item.title}</Text>
+        <Image style={styles.introImageStyle} source={item.image} />
+        <Text style={styles.introTextStyle}>{item.text}</Text>
+      </View>
+    );
+  };
+
   return (
-    <div>AppIntro</div>
-  )
-}
+    <>
+      {showRealApp ? (
+        <SafeAreaView style={styles.container}>
+          <View style={styles.container}>
+            <Text style={styles.titleStyle}>
+              React Native App Intro Slider using AppIntroSlider
+            </Text>
+            <Text style={styles.paragraphStyle}>
+              This will be your screen when you click Skip from any slide or
+              Done button at last
+            </Text>
+            <Button
+              title="Show Intro Slider again"
+              onPress={() => setShowRealApp(false)}
+            />
+          </View>
+        </SafeAreaView>
+      ) : (
+        <AppIntroSlider
+          data={slides}
+          renderItem={RenderItem}
+          onDone={onDone}
+          showSkipButton={true}
+          onSkip={onSkip}
+        />
+      )}
+    </>
+  );
+};
 
-export default AppIntro
+export default AppIntro;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    padding: 10,
+    justifyContent: 'center',
+  },
+  titleStyle: {
+    padding: 10,
+    textAlign: 'center',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  paragraphStyle: {
+    padding: 20,
+    textAlign: 'center',
+    fontSize: 16,
+  },
+  introImageStyle: {
+    width: 200,
+    height: 200,
+  },
+  introTextStyle: {
+    fontSize: 18,
+    color: 'white',
+    textAlign: 'center',
+    paddingVertical: 30,
+  },
+  introTitleStyle: {
+    fontSize: 25,
+    color: 'white',
+    textAlign: 'center',
+    marginBottom: 16,
+    fontWeight: 'bold',
+  },
+});
+
+const slides = [
+  {
+    key: 's1',
+    text: 'Best Recharge offers',
+    title: 'As a Provider...',
+    image: {
+      uri:
+        'https://raw.githubusercontent.com/AboutReact/sampleresource/master/intro_mobile_recharge.png',
+    },
+    backgroundColor: '#3585f6',
+  },
+  {
+    key: 's2',
+    title: 'As a Seeker...',
+    text: 'Upto 25% off on Domestic Flights',
+    image: {
+      uri:
+        'https://raw.githubusercontent.com/AboutReact/sampleresource/master/intro_flight_ticket_booking.png',
+    },
+    backgroundColor: '#3585f6',
+  },
+  {
+    key: 's3',
+    title: 'Start your journey\nto a greater you\ntoday.',
+    text: 'Enjoy Great offers on our all services',
+    image: {
+      uri:
+        'https://raw.githubusercontent.com/AboutReact/sampleresource/master/intro_discount.png',
+    },
+    backgroundColor: '#3585f6',
+  },
+];
