feef1fdcd44e3b8454e8f6ef4abc6afb863b6979
Jordan-Burgess <alfredjordan93@gmail.com>
2023-05-10 18:29:38 -0400

    Add interest choices data

--- components/OnboardingInterestForm.js
+++ components/OnboardingInterestForm.js
@@ -1,7 +1,29 @@
 import {useState} from 'react'
 import { View, Text } from 'react-native'
 
-function OnboardingInterestForm() {
+services = {
+  cosmotology : [
+    'Electrologist', 'Celebrity MUA', 'Nail Tech', 'Hairstylist',
+    'Barber', 'Makeup Artist', 'Beauty Writer', 'Fashion Show Stylist',
+    'Lash Tech', 'Aesthetician', 'Braider', 'Loctician',
+  ],
+  fitness : [
+    'Tai Chi', 'Calisthenics', 'MMA', 'Strength and Conditioning',
+    'Pilates Instructor', 'USAW', 'Boxing Coach', 'Personal Trainer',
+    'Golf Specialist', 'Crossfit Trainer',
+  ],
+  education : [
+    'Fitness', 'Health', 'Nutrition',
+  ],
+  holistic : [
+    'Alignment', 'Hot Yoga Classes', 'Massage Therapist', 'Herbalist',
+    'Acupuncturist', 'Reflexologist', 'Ayervedic Physitian', 'Prosthetist',
+    'Physical Therapist', 'Dietician',
+  ],
+}
+
+
+function OnboardingInterestForm({service}) {
   const [interest, setInterest] = useState('')
   return (
     <View>
@@ -11,6 +33,12 @@ function OnboardingInterestForm() {
         onChangeText={setInterest}
         value={interest}
         placeholder='Browse a little!'
+      />
+      <Button
+       title='Go Back'
+      />
+      <Button
+       title='Continue'
       />
     </View>
   )
